ad9035cfbf0d3091f62fa85a83b3484296f51052
Matt Atkins <matt@yoomee.com>
2011-07-19 16:45:39 +0100

    Removing unused images. Style changes.

--- lib/yoomee_content_flags.rb
+++
@@ -1,9 +0,0 @@
-module YoomeeContentFlags
-  
-  def self.included(klass)
-    Formtastic::SemanticFormBuilder.send(:include, YoomeeContentFilter::SemanticFormBuilderExtensions)
-    User.send(:include, YoomeeContentFilter::UserExtensions)
-    Notifier.send(:include, YoomeeContentFlags::NotifierExtensions)
-  end
-  
-end
