d08f58d560d81eb411beb3301fff33620b68a759
Greg Woodcock <greg@yoomee.com>
2015-02-13 11:08:11 +0000

    Change the render method to return a js view rather than an inline block

--- app/controllers/moderation_base_controller.rb
+++ app/controllers/moderation_base_controller.rb
@@ -42,33 +42,26 @@ class ModerationBaseController < ApplicationController
       raise CanCan::AccessDenied, "You're not allowed to access this page"
     end
   end
-  
+
   def open_action?
     self.class::open_action?(action_name)
   end
 
   protected
   def replace_moderation_content(partial_name, options={})
-    wrapper_class = options.delete(:wrapper_class)
+    byebug
+    @partial_name = partial_name
+    @options = options
+    @wrapper_class = options.delete(:wrapper_class) || 'test'
     if !(active_color = options.delete(:active_color))
        active_color = @content_flag_type.nil? ? "#444" : @content_flag_type.color
     end
-    
-    render :update do |page|
-      page << "$('#moderation_right_inner').html('#{escape_javascript(render(partial_name, options))}');"
-      page << "$('#moderation_sidebar').replaceWith('#{escape_javascript(render("moderation/sidebar"))}');"
-      page << "$('#moderation_wrapper').attr('class', '#{escape_javascript(wrapper_class)}');"
-      if active_color
-        page << "$('#moderation_right_col').css('border-color', '#{active_color}');"
-      end
-      page << "ModerationContent.finished();"
-      
-    end
+    render 'content_flags/update', :status => 200
   end
-  
+
   def set_up_sidebar
-    @inbox_count = ContentFlag.unresolved.count
+    @inbox_count = ContentFlag.unresolved.to_a.count
     @content_flag_types ||= ContentFlagType.all
   end
-  
+
 end
