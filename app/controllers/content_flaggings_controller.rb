89fe1fae751adf418da8d589c039111493a33e95
David Thompson <dtt101@gmail.com>
2015-03-13 15:47:20 +0000

    Fix content flaggings for Rails 4

--- app/controllers/content_flaggings_controller.rb
+++ app/controllers/content_flaggings_controller.rb
@@ -1,20 +1,23 @@
 class ContentFlaggingsController < ModerationBaseController
-    
+
   def new
     @content_flagging = ContentFlagging.new(:attachable_type => params[:attachable_type], :attachable_id => params[:attachable_id], :url => params[:url])
     render :partial => "content_flaggings/form", :locals => {:content_flagging => @content_flagging}
   end
-  
+
   def create
-    @content_flagging = ContentFlagging.new(params[:content_flagging].merge(:user => current_user, :ip_address => request.remote_ip))
-    render :update do |page|
-      if @content_flagging.save
-        page << "$('#content_flag_form_wrapper').replaceWith('#{escape_javascript(render("content_flaggings/success"))}');"
-      else
-        page << "$('#content_flag_box').replaceWith('#{escape_javascript(render("content_flaggings/form", :content_flagging => @content_flagging))}');"
-      end
-      page << "$.fancybox.resize();"
+    @content_flagging = ContentFlagging.new(content_flagging_params.merge(:user => current_user, :ip_address => request.remote_ip))
+    @success = @content_flagging.save
+    respond_to do |format|
+      format.js
     end
+
   end
-  
+
+  private
+
+  def content_flagging_params
+    params.require(:content_flagging).permit(:attachable_type, :attachable_id, :url, :message, :email, :content_flag_type_id)
+  end
+
 end
