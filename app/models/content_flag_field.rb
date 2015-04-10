423266d61610e040e45e302c20124d089c1fdcb0
Edward Andrews-Hodgson <edward@yoomee.com>
2015-04-10 10:21:16 +0100

    Update to rails 4 scopes

--- app/models/content_flag_field.rb
+++ app/models/content_flag_field.rb
@@ -2,14 +2,14 @@ class ContentFlagField < ActiveRecord::Base
 
   belongs_to :content_flag
   validates_presence_of :name
-  
+
   scope :latest, -> { order("created_at DESC") }
-  scope :name_is, lambda{|name| {:conditions => {:name => name}}}
-  scope :name_is_not, lambda{|name| {:conditions => ["name != ?", name]}}
-  
+  scope :name_is, -> (name) { where(:name => name) }
+  scope :name_is_not, -> (name) { where.not(:name => name) }
+
   def name=(val)
     val = val.to_s if val.is_a?(Symbol)
     write_attribute(:name, val)
   end
-  
+
 end
