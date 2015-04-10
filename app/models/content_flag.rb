6aa6fc44d37d2a85c8bdee3184f7792d31de36cb
Edward Andrews-Hodgson <edward@yoomee.com>
2015-04-10 12:40:42 +0100

    Fix average_response_in_seconds on content flag

--- app/models/content_flag.rb
+++ app/models/content_flag.rb
@@ -4,7 +4,7 @@ class ContentFlag < ActiveRecord::Base
   belongs_to :attachable, :polymorphic => true
   belongs_to :resolved_by, :class_name => "User"
   before_create :set_opened_at
-  
+
   has_many :content_flag_fields, :autosave => true, :dependent => :destroy
   has_many :content_flaggings, :dependent => :destroy
   has_many :content_flag_types, :through => :content_flaggings
@@ -18,13 +18,13 @@ class ContentFlag < ActiveRecord::Base
   scope :ascend_by_resolved_at, -> {order("content_flags.resolved_at ASC") }
   scope :descend_by_resolved_at, -> { order("content_flags.resolved_at DESC") }
   scope :not_including, ->(content_flag) { (content_flag.nil? || content_flag.id.nil?) ? all : where("content_flags.id != #{ content_flag.id }")}
-  
+
   class << self
-    
+
     def average_response_in_seconds
-      resolved.average("TIME_TO_SEC(TIMEDIFF(content_flags.resolved_at,content_flags.opened_at))").to_i
+      resolved.average('EXTRACT(EPOCH FROM (resolved_at - created_at))').to_i
     end
-    
+
     def average_response
       time = average_response_in_seconds
       return time, "second" if time < 600
@@ -35,13 +35,13 @@ class ContentFlag < ActiveRecord::Base
       time = time/24
       return time, "day"
     end
-    
+
     def unresolved_count
       unresolved.count(:id, :distinct => true)
     end
-    
+
   end
-  
+
   def create_content_flag_field_if_changed(attribute, value)
     latest_flag = content_flag_fields.name_is(attribute.to_s).latest.first
     if latest_flag.blank?
@@ -54,7 +54,7 @@ class ContentFlag < ActiveRecord::Base
   def has_attachable?
     !attachable.nil?
   end
-  
+
   def history(field = nil)
     h = []
     h += content_flaggings
@@ -74,7 +74,7 @@ class ContentFlag < ActiveRecord::Base
       attachable.respond_to?(:user) ? attachable.user : nil
     end
   end
-  
+
   def user_full_name
     user.nil? ? nil : user.full_name
   end
@@ -89,7 +89,7 @@ class ContentFlag < ActiveRecord::Base
     end
   end
   alias_method :to_s, :name
-  
+
   def next(options = {})
     if options[:menu_item]=="resolved"
       return ContentFlag.resolved.descend_by_resolved_at.find(:first, :conditions => ["content_flags.resolved_at < ?", resolved_at])
@@ -98,7 +98,7 @@ class ContentFlag < ActiveRecord::Base
     next_content_flags = content_flag_type.nil? ? ContentFlag : content_flag_type.content_flags
     next_content_flags.unresolved.where("content_flags.created_at < ?", self.created_at).not_including(self).order(created_at: :desc).first
   end
-  
+
   def prev(options = {})
     if options[:menu_item]=="resolved"
       return ContentFlag.resolved.ascend_by_resolved_at.find(:first, :conditions => ["content_flags.resolved_at > ?", resolved_at])
@@ -107,7 +107,7 @@ class ContentFlag < ActiveRecord::Base
     prev_content_flags = content_flag_type.nil? ? ContentFlag : content_flag_type.content_flags
     prev_content_flags.unresolved.where("content_flags.created_at > ?", self.created_at).not_including(self).order(created_at: :asc).first
   end
-  
+
   def text_field
     if has_attachable?
       text_fields = content_flag_fields.reject{|f| ["user_id","attachable_type"].include?(f.name)}
@@ -119,25 +119,25 @@ class ContentFlag < ActiveRecord::Base
         else
           text_fields.first
         end
-      end      
+      end
     end
   end
-  
+
   def current_text
     if text_field
       attachable.send(text_field.name)
     end
   end
-  
+
   def resolve!(user)
     return nil if user.nil?
     self.update_attributes(:resolved_by => user, :resolved_at => Time.now)
   end
-  
+
   def unresolve!
     self.update_attributes(:resolved_by => nil, :resolved_at => nil, :opened_at => Time.now)
   end
-  
+
   def resolved
     !resolved_at.blank?
   end
@@ -151,8 +151,8 @@ class ContentFlag < ActiveRecord::Base
 
   def text
     text_field ? Iconv.conv('ASCII//IGNORE', 'UTF8', text_field.value) : nil
-  end  
-  
+  end
+
   private
   def set_opened_at
     self.opened_at = Time.now
