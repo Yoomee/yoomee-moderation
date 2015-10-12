1503b11333666b55f680aa2f1ba2a3af1ba8ce29
Greg Woodcock <greg@yoomee.com>
2015-10-12 14:27:44 +0100

    Remove a post or comment if 3 different people report it
    Previous behaviour allowed a user to report a post multiple times to
    remove it as long as 1 other person had reported it

--- app/models/content_flagging.rb
+++ app/models/content_flagging.rb
@@ -18,7 +18,7 @@ class ContentFlagging < ActiveRecord::Base
 
   before_create :create_content_flag_fields_and_unresolve
   before_validation :build_content_flag, :on => :create
-  before_save :remove_post_or_comment_if_needed
+  after_save :remove_post_or_comment_if_needed
   after_create :send_email
 
   scope :created_at_greater_than, -> (date) { where('content_flaggings.created_at > ?', date) }
@@ -97,7 +97,7 @@ class ContentFlagging < ActiveRecord::Base
 
   def remove_post_or_comment_if_needed
     if attachable && (attachable.is_a?(Post) || attachable.is_a?(Comment))
-      if !attachable.removed? && attachable.content_flaggings.select("content_flaggings.user_id").where("content_flaggings.user_id IS NOT NULL AND content_flaggings.user_id != ?",User.elephant.id).group("content_flaggings.user_id").to_a.size >= 2
+      if !attachable.removed? && attachable.content_flaggings.select("content_flaggings.user_id").where("content_flaggings.user_id IS NOT NULL AND content_flaggings.user_id != ?",User.elephant.id).group("content_flaggings.user_id").to_a.size >= 3
         attachable.update_attribute(:removed, true)
       end
     end
